cd75125fa5ee6af40cb49d0221735a959f315d52
Caryl Stuart <carylstuart@email.com>
2017-11-04 19:15:39 -0600

    Recreated package and made changes to references.

---
+++ app/src/main/java/edu/cnm/deepdive/mytrainerandi/MainActivity.java
@@ -0,0 +1,145 @@
+package edu.cnm.deepdive.mytrainerandi;
+
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+
+import android.app.Fragment;
+
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+public class MainActivity extends AppCompatActivity
+    implements NavigationView.OnNavigationItemSelectedListener {
+
+  public static final String DATABASE_NAME = "mydatabase";
+
+  SQLiteDatabase mDatabase;
+
+  EditText editTextName;
+  Spinner spinnerGoal;
+
+  /**
+   * On Create
+   * @param savedInstanceState
+   */
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+    Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+    setSupportActionBar(toolbar);
+
+    DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+    ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+        this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+    drawer.setDrawerListener(toggle);
+    toggle.syncState();
+
+    NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+    navigationView.setNavigationItemSelectedListener(this);
+
+    /**
+     * SQLite Database
+     */
+    mDatabase = openOrCreateDatabase(DATABASE_NAME, MODE_PRIVATE, null);
+    createTable();
+    spinnerGoal = (Spinner) findViewById(R.id.spinnerGoal);
+  }
+
+  private void createTable() {
+    String sql = "CREATE TABLE client (\n"
+        + "    id INTEGER NOT NULL CONSTRAINT client_pk PRIMARY KEY AUTOINCREMENT,\n"
+        + "    name varchar(200) NOT NULL,\n"
+        + "    goal varchar(200) NOT NULL,\n"
+        + "    entrydate datetime NOT NULL,\n"
+        + "    weight double NOT NULL,\n"
+        + "    height double NOT NULL,\n"
+        + "    bmi double NOT NULL\n"
+        + ");";
+    mDatabase.execSQL(sql);
+  }
+
+
+  @Override
+  public void onBackPressed() {
+    DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+    if (drawer.isDrawerOpen(GravityCompat.START)) {
+      drawer.closeDrawer(GravityCompat.START);
+    } else {
+      super.onBackPressed();
+    }
+  }
+
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    // Inflate the menu; this adds items to the action bar if it is present.
+    getMenuInflater().inflate(R.menu.main, menu);
+    return true;
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    // Handle action bar item clicks here. The action bar will
+    // automatically handle clicks on the Home/Up button, so long
+    // as you specify a parent activity in AndroidManifest.xml.
+    int id = item.getItemId();
+
+    //noinspection SimplifiableIfStatement
+    if (id == R.id.action_settings) {
+      return true;
+    }
+
+    return super.onOptionsItemSelected(item);
+  }
+
+  private void displaySelectedScreen(int id) {
+    //FragmentManager fragment = getSupportFragmentManager();
+
+    Fragment fragment = null;
+    id = getIntent().getIntExtra("position", id);
+
+    switch (id) {
+      case R.id.nav_home:
+        fragment = new Home1();
+        break;
+      case R.id.nav_day:
+        fragment = new Day2();
+        break;
+      case R.id.nav_trainer:
+        fragment = new Trainer3();
+        break;
+      case R.id.nav_client:
+        fragment = new Client4();
+        break;
+    }
+    if (fragment != null) {
+      android.app.FragmentTransaction ft = getFragmentManager().beginTransaction();
+      ft.replace(R.id.content_main, fragment);
+      ft.commit();
+    }
+
+
+    DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+    drawer.closeDrawer(GravityCompat.START);
+  }
+
+  @SuppressWarnings("StatementWithEmptyBody")
+  @Override
+  public boolean onNavigationItemSelected(MenuItem item) {
+    // Handle navigation view item clicks here.
+    int id = item.getItemId();
+
+    displaySelectedScreen(id);
+
+    return true;
+  }
+}
---
+++ app/src/main/java/edu/cnm/deepdive/mytrainerandi/Trainer3.java
@@ -0,0 +1,22 @@
+package edu.cnm.deepdive.mytrainerandi;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Trainer3 extends Fragment {
+
+  @Override
+  public View onCreateView(LayoutInflater inflater, ViewGroup container,
+      Bundle savedInstanceState) {
+   return inflater.inflate(R.layout.trainer3, container, false);
+  }
+
+  @Override
+  public void onViewCreated(View view, Bundle savedInstanceState) {
+    super.onViewCreated(view, savedInstanceState);
+    getActivity().setTitle("Trainer Plan");
+  }
+}
