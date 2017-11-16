24cc682e2599462016d2e8f6138ea05e293c5ab3
Caryl Stuart <carylstuart@email.com>
2017-11-16 06:19:38 -0700

    Changed Trainer 3 to handle checkbox selections.

--- app/src/main/java/edu/cnm/deepdive/mytrainerandi/Trainer3.java
+++ app/src/main/java/edu/cnm/deepdive/mytrainerandi/Trainer3.java
@@ -1,5 +1,8 @@
 package edu.cnm.deepdive.mytrainerandi;
 
+import static com.j256.ormlite.android.apptools.OpenHelperManager.helper;
+import static edu.cnm.deepdive.mytrainerandi.entity.Exercise.CIRCUIT_COLNAME;
+
 import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -23,11 +26,13 @@ public class Trainer3 extends Fragment implements OnCheckedChangeListener {
 
     View trainerView = inflater.inflate(R.layout.trainer3, container, false);
 
+    //CheckBox generates an Event Source-state change.
     CheckBox cbAbs = trainerView.findViewById(R.id.cb_abs);
     CheckBox cbCardio = trainerView.findViewById(R.id.cb_cardio);
     CheckBox cbLower = trainerView.findViewById(R.id.cb_lower);
     CheckBox cbUpper = trainerView.findViewById(R.id.cb_upper);
 
+    //Checkbox Event Listener is notified when an event occurs.
     cbAbs.setOnCheckedChangeListener(this);
     cbCardio.setOnCheckedChangeListener(this);
     cbLower.setOnCheckedChangeListener(this);
@@ -41,34 +46,13 @@ public class Trainer3 extends Fragment implements OnCheckedChangeListener {
     getActivity().setTitle(R.string.trainer_plan);
   }
 
-  //?Added
-//  private void refresh(int dayofweek) {         //need this to check circuit which is a string
-//    List<Exercise> allexercise = null;
-//    try {
-//      allexercise = helper
-//          .getDayscheduleDao().queryForEq(DAYOFWEEK, dayofweek);
-//    } catch (SQLException e) {
-//      e.printStackTrace();
-//    }
-
-//.getExerciseDao().queryForEq(CIRCUIT_COLNAME, circuit);           //defined in Exercise.java
-//    } catch (SQLException e) {
-//      e.printStackTrace();
-//    }
-
-  private void refresh(int dayofweek) {
-
-    //Display exercises.
-    Trainer3ListAdapter trainer3Adapter = new Trainer3ListAdapter(getActivity(),
-        R.layout.trainer3_listview, dayschedule);
-    ListView trainer3View = (ListView) inflate.findViewById(R.id.listViewTrainer3);
-    trainer3View.setAdapter(trainer3Adapter);
-  }
+  //CompoundButton: A button with two states, checked and unchecked. When the button is pressed or clicked,
+  // the state changes automatically.
 
   @Override
   public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
     //swith on compoundButton
-    //check if they check more than one
+    //check if they check more than one--online they suggest to use radio button cuz you can group and only allow one.?
     //call refresh mehto
 
     if (isChecked) {
@@ -84,11 +68,29 @@ public class Trainer3 extends Fragment implements OnCheckedChangeListener {
       if (compoundButton == cbUpper) {
         showTextNotification("Upper");
       }
+      refresh(xx);
     }
   }
-  
+
+  //Pop up with which check box was picked using Toast.
   public void showTextNotification(String msgToDisplay) {
     Toast.makeText(this, msgToDisplay, Toast.LENGTH_SHORT).show();
+  }
+
+  private void refresh(int trainView) {
+    List<Exercise> allexercise = null;
+    try {
+      allexercise = helper
+          .getExerciseDao().queryForEq(CIRCUIT_COLNAME, circuit);
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+
+    //Display exercises.
+    Trainer3ListAdapter trainer3Adapter = new Trainer3ListAdapter(getActivity(),
+        R.layout.trainer3_listview, dayschedule);
+    ListView trainer3View = (ListView) inflate.findViewById(R.id.listViewTrainer3);
+    trainer3View.setAdapter(trainer3Adapter);
   }
 }
 
